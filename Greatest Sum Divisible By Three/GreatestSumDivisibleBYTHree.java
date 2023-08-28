4e09a95ada88fa80bd8b22e90e7f796fb56f9ada
Harshita Rawat <harshita.reenu184@gmail.com>
2023-08-28 12:41:50 +0530

    Update GreatestSumDivisibleBYTHree.java

--- Greatest Sum Divisible By Three/GreatestSumDivisibleBYTHree.java
+++ Greatest Sum Divisible By Three/GreatestSumDivisibleBYTHree.java
@@ -1,16 +1,22 @@
 class Solution {
     public int maxSumDivThree(int[] nums) {
-      int sum=0, subValue=0;
-      for(int i=0;i<nums.length;i++){
-          sum=sum+nums[i];
-      }  
-      if(sum%3==0)
-      {
-          return sum;
-      }
-      else
-      {
-          
-      }
+        int n=nums.length;
+        int[] sums=new int [3];
+
+        for(int i=0; i<n; i++){
+            int s0=sums[0]+nums[i];
+            int s1=sums[1]+nums[i];
+            int s2=sums[2]+nums[i];
+
+            int rim0=s0 % 3;
+            int rim1=s1 % 3;
+            int rim2=s2 % 3;
+
+            sums[rim0]=Math.max(sums[rim0], s0);
+            sums[rim1]=Math.max(sums[rim1], s1);
+            sums[rim2]=Math.max(sums[rim2], s2);
+        } 
+
+        return sums[0];
     }
 }
